dc140a924cad95e6176ca63d7f5dbce370f00cd5
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-10-18 22:31:08 +0200

    go to a better versionning

--- resources/extra.sh
+++ resources/extra.sh
@@ -5,17 +5,17 @@ set -x
 echo "Kernel extras for ${KERNEL_VERSION}"
 
 #DIR=/lib/modules/${KERNEL_VERSION}/build
-STAMP=/lib/modules/${KERNEL_VERSION}/.extras-${EXTRA_TAG}-done
+STAMP=/lib/modules/${KERNEL_VERSION}/.extras-${EXTRA_VERSION}-done
 
 if [ -e $STAMP ]; then
-    echo Kernel ${EXTRA_TAG} extras for ${KERNEL_VERSION} already installed. Delete $STAMP to reinstall
+    echo Kernel ${EXTRA_VERSION} extras for ${KERNEL_VERSION} already installed. Delete $STAMP to reinstall
     exit 0
 fi
 
-cat /extras-${EXTRA_TAG}.tar.gz | gzip -dc | tar xf - -C /
+cat /extras-${EXTRA_VERSION}.tar.gz | gzip -dc | tar xf - -C /
 if [ "${KERNEL_VERSION}" == "$(uname -r)" ]; then
     depmod -a
 fi
 touch $STAMP
 
-echo Kernel ${EXTRA_TAG} extras for ${KERNEL_VERSION} installed. Delete $STAMP to reinstall
+echo Kernel ${EXTRA_VERSION} extras for ${KERNEL_VERSION} installed. Delete $STAMP to reinstall
